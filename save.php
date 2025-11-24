54ce3a5a0db10adbeda6992b74caa519b90912c9
kiank0277-ops <kiank0277@gmail.com>
2025-11-24 19:44:17 +0330

    Update save.php

--- save.php
+++ save.php
@@ -2,7 +2,6 @@
 
 function deviceType() {
     $agent = $_SERVER['HTTP_USER_AGENT'];
-
     if (preg_match('/mobile/i', $agent)) {
         return "موبایل";
     } else {
@@ -17,10 +16,15 @@ $code = $_POST['code'] ?? '';
 
 $time = date("Y-m-d H:i:s");
 $device = deviceType();
+$ip = $_SERVER['REMOTE_ADDR']; // آی‌پی کاربر
 
+// ذخیره در data.txt
 $data = "نام: $name | نام خانوادگی: $lastname | شماره: $phone | کد: $code | دستگاه: $device | زمان: $time\n";
-
 file_put_contents("data.txt", $data, FILE_APPEND);
+
+// ذخیره در log.txt
+$log = "نام: $name | نام خانوادگی: $lastname | شماره: $phone | کد: $code | دستگاه: $device | آی‌پی: $ip | زمان: $time\n";
+file_put_contents("log.txt", $log, FILE_APPEND);
 
 echo "saved";
 ?>
